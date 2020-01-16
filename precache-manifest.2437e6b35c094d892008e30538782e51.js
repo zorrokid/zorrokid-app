c77988159ab00c3cd26b1574297d6652bb00c38c
Mikko Keinänen <mikko.keinanen@granlund.fi>
2020-01-16 20:09:54 +0200

    Updates

---
+++ precache-manifest.2437e6b35c094d892008e30538782e51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957083757ad99e9752e9bf871a8569c3",
+    "url": "/zorrokid-app/index.html"
+  },
+  {
+    "revision": "77fbdf9c8e235b1481d6",
+    "url": "/zorrokid-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bac4226520fab8fe0a30",
+    "url": "/zorrokid-app/static/js/2.d35ee289.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/zorrokid-app/static/js/2.d35ee289.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77fbdf9c8e235b1481d6",
+    "url": "/zorrokid-app/static/js/main.8bcf208e.chunk.js"
+  },
+  {
+    "revision": "2a259e2caa49f0b2e4ea",
+    "url": "/zorrokid-app/static/js/runtime-main.7fa68cc7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/zorrokid-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
